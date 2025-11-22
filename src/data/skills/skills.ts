c8c9a36c93d62a257428a095600458047c5afd7e
Mariano Vega <marianovh2000@gmail.com>
2025-11-22 14:04:03 -0500

    Agregar skills a las card de stacks

--- src/data/skills/skills.ts
+++ src/data/skills/skills.ts
@@ -3,9 +3,9 @@ import { Code, Server, Database, Terminal } from 'lucide-react';
 import type { Skill } from '../../interfaces/app/app';
 
 const skills: Skill[] = [
-  { name: 'Frontend', icon: Code, tools: 'React, TypeScript, Tailwind, Next.js' },
-  { name: 'Backend', icon: Server, tools: 'Node.js, Express, Python, Django' },
-  { name: 'Database', icon: Database, tools: 'PostgreSQL, MongoDB, Redis' },
+  { name: 'Frontend', icon: Code, tools: 'React, TypeScript, Tailwind, React Native' },
+  { name: 'Backend', icon: Server, tools: 'Node, Express, Nest, Python, Web Scraping' },
+  { name: 'Database', icon: Database, tools: 'PostgreSQL, SQL Server, MySQL,SQLite, MongoDB, Dynamo' },
   { name: 'DevOps', icon: Terminal, tools: 'Docker, AWS, Git Actions, Linux' }
 ];
 
